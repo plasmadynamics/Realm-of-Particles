3f927327942b4fb3b184270c3a0232e45aeb879e
Rasmus 'thePlasm' Jepsen <rasmus.jepsen@gmail.com>
2014-12-26 18:53:09 +1000

    gameloop

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-var c = document.getElementById("johnnythecanvas");
-var ctx = c.getContext("2d");
-ctx.fillStyle = "#000000";
+function gameLoop() {
+	var running = true;
+	while (running) {
+		var c = document.getElementById("johnnythecanvas");
+		var ctx = c.getContext("2d");
+		ctx.fillStyle = "#000000";
+	}
+}
