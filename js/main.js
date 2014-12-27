a8a50ad9a60496976f112eae4e3f0b6dddd9d250
Rasmus 'thePlasm' Jepsen <rasmus.jepsen@gmail.com>
2014-12-27 14:03:36 +1000

    YAY ME GET CANVAS WERK!!!1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,45 @@
-function gameLoop() {
-	var running = true;
-	while (running) {
-		var c = document.getElementById("johnnythecanvas");
-		var ctx = c.getContext("2d");
-		ctx.fillStyle = "#000000";
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var char = document.getElementById("char");
+var charx = 0.0;
+var chary = 0.0;
+ctx.fillStyle = '#FFFFFF';
+ctx.fillRect(0,0,canvas.width,canvas.height);
+ctx.drawImage(char, 0, 0, 14, 16, charx, chary, 14, 16);
+function keys(event) {
+	switch (event.keyCode) {
+		case 87: //W
+			if (chary > 0) {
+				chary--;
+				ctx.fillRect(0,0,canvas.width,canvas.height);
+				ctx.drawImage(char, 0, 0 , 14, 16, charx, chary, 14, 16);
+				break;
+			}
+			break;
+		case 65: //A
+			if (charx > 0) {
+				charx--;
+				ctx.fillRect(0,0,canvas.width,canvas.height);
+				ctx.drawImage(char, 0, 0, 14, 16, charx, chary, 14, 16);
+				break;
+			}
+			break;
+		case 83: //S
+			if (chary+16 < canvas.height) {
+				chary++;
+				ctx.fillRect(0,0,canvas.width,canvas.height);
+				ctx.drawImage(char, 0, 0, 14, 16, charx, chary, 14, 16);
+				break;
+			}
+			break;
+		case 68: //D
+			if (charx+14 < canvas.width) {
+				charx++;
+				ctx.fillRect(0,0,canvas.width,canvas.height);
+				ctx.drawImage(char, 0, 0, 14, 16, charx, chary, 14, 16);
+				break;
+			}
+			break;
 	}
 }
+window.addEventListener('keydown', keys, false);
